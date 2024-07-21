31fe1f4ec7d78a5e033e972ffaeec64db3f96fef
Biniam Aregay <seifubiniam6@gmail.com>
2024-07-21 23:11:53 +0300

    add a class using classList.add

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Create a remove button
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
-    removeButton.className = "remove-btn";
+    removeButton.classList.add("remove-btn");
 
     // Assign an event to the remove button to remove the task
     removeButton.onclick = function () {
